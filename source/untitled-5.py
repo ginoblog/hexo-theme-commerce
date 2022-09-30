e545a6f85ee2973527e4d615da47ff1e05d0dab4
ginoblog <gino_redlight@163.com>
2022-09-30 16:47:34 +0800

    update proj

--- source/untitled-5.py
+++ source/untitled-5.py
@@ -3,5 +3,5 @@ from PIL import Image
 wr = Image.open("images/flow2.jpeg")
 print(wr.height,wr.width)
 
-wt = wr.resize((130*4,130*3),Image.LANCZOS)
+wt = wr.resize((130*4,130*3  ),Image.LANCZOS)
 wt.save("images/flow.jpeg")
